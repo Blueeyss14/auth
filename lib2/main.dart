06222c56a86641181c89ad8f0f3a221a6f8b5b15
unknown <delkanoberutu8@gmail.com>
2024-08-28 23:34:08 +0700

    lib2

--- lib2/main.dart
+++ lib2/main.dart
@@ -1,3 +1,5 @@
+//lib2
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -8,16 +10,15 @@ import 'package:regist_login2/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
-  runApp(
-    MultiProvider(providers: [
-      ChangeNotifierProvider(create: (_) => AuthServices(),)
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (_) => AuthServices(),
+      )
     ],
-      child: const MyApp(),
-    )
-  );
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {

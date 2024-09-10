e892cfddfbbaffa59fb6d612131a709a2fc03e94
unknown <delkanoberutu8@gmail.com>
2024-09-11 02:49:14 +0700

    fix null

--- lib3/home/home.dart
+++ lib3/home/home.dart
@@ -1,36 +1,64 @@
+import 'package:c_test_firebase/pages/chat/chat.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:regist_login3/auth/providers/auth_provider.dart';
+import '../Auth/provider/auth_provider.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  void signOut() async {
-    final authProviderService = Provider.of<AuthProviderService>(context, listen: false);
-
-    await authProviderService.signOut();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthServicesProvider>(context);
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Login Successful"),
-            const SizedBox(height: 20),
-            GestureDetector(
-              onTap: signOut,
-                child: Icon(Icons.logout))
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text("Users"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              authProvider.signOut();
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder<QuerySnapshot>(
+        future: FirebaseFirestore.instance.collection('users').get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('No User Found'));
+          }
+
+          final users = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              final user = users[index].data() as Map<String, dynamic>;
+              return ListTile(
+                title: Text(user['email']),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          ChatPage(receiverId: user['userId']),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
       ),
     );
   }
